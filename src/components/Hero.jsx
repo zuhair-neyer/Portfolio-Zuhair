2a79684f36016cc6ff64d9eb33d44cba9ac9bb89
zuhair-neyer <zuhairmumtaz87@gmail.com>
2025-07-26 15:33:45 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,10 +15,10 @@ const Hero = () => {
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white `}>
-            Hi, I'm <span className="text-[#915eff]">Swastik</span>
+            Hi, I'm <span className="text-[#915eff]">Zuhair</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white`}>
-            i develop 3d visuals, user <br className="sm:block hidden" />{" "}
+            I develop 3d visuals, user <br className="sm:block hidden" />{" "}
             interfaces and web applications
           </p>
         </div>
