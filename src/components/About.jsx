9af257212c452a1048fe235d6ace23d61ad2dd19
zuhair-neyer <zuhairmumtaz87@gmail.com>
2025-07-26 15:27:45 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -51,12 +51,10 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[14px] sm:text-[17px] max-w-3xl sm:leading-[30px] leading-1"
       >
-        Hi, I’m Swastik Yadav — a passionate Frontend / Full Stack Developer
+        Hi, I’m Zuhair Mumtaz — a passionate Frontend / Full Stack Developer
         with hands-on experience in building modern, responsive web applications
-        using technologies like React.js, Tailwind CSS, Node.js, and MongoDB.
-        I’ve worked on real-world freelance projects and participated in
-        hackathons like ReImagine, where I pushed my creativity and technical
-        skills to the next level.
+        using technologies like React.js, Tailwind CSS and Node.js.
+        I’ve worked on real-world personal projects.
       </motion.p>
       <div className="mt-20 flex  flex-wrap gap-10">
         {services.map((service, index) => (
